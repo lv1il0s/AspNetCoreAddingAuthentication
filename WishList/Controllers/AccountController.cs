1f26ec546e63f8b871b44551d5ca153b7ca355ef
Milosevic Milos <Milos.Milosevic@zumtobelgroup.com>
2021-11-23 20:16:49 +0100

    New: "Create Private Fields For AccountController"
    Create private fields in the AccountController class.
    
    This should have a private readonly field of type UserManager<ApplicationUser> named _userManager (you will need using directives for WishList.Models and Microsoft.AspNetCore.Identity)
    This should have a private readonly field of type SignInManager<ApplicationUser> named _signInManager

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WishList.Models;
 
 namespace WishList.Controllers
 {
     [Authorize]
     public class AccountController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
         public IActionResult Index()
         {
             return View();
